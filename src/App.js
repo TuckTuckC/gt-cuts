40ed7802fe85e7fd7a112339703d0ffea98a6253
Tucker Craig <tuckermcraig@gmail.com>
2022-10-05 22:36:50 -0400

    Created portfolio component and added info to home

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
     <div>
-      Hello from Home!
+      <h1>GT Streamlined</h1>
+      <h2>What We Do</h2>
+      <p>Input mission statement here</p>
+      <h3>Contact Info</h3>
+      <p>Info</p>
+      {/* Contact Form */} 
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
