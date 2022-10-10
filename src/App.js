59e6cd5180644ea55355f176c73b595e6106c5bd
Tucker Craig <tuckermcraig@gmail.com>
2022-10-10 15:16:20 -0400

    Deletes Portfolio Fills NavBar Starts Styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route, Routes } from 'react-router-dom';
 // Components
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
-import Portfolio from './Components/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
     </div>
   );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div>
+    <div className='Navbar'>
       <a href="/">Home</a>
-      <a href="/portfolio">Portfolio</a>
+      <a href="/lawn-care">Lawn Care</a>
+      <a href="/">Yard Clean Up</a>
+      <a href="/">Tree Services</a>
+      <a href="/">Concrete</a>
+      <a href="/">Snow Removal</a>
     </div>
   );
 };
