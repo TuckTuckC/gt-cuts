9723095173b14a3e0fa5566e28205cd2ffa0b904
Tucker Craig <tuckermcraig@gmail.com>
2022-11-01 21:51:03 -0400

    Adds actual information to the homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,10 +5,34 @@ function Home() {
   return (
     <div className='Home'>
       <h1>GT Streamlined</h1>
-      <h2>What We Do</h2>
-      <p>Input mission statement here</p>
+      <h2>Bumbcombe & Haywood</h2>
+      <h3>"We will bring fresh and innovative ideas and solutions, along with a variety of quality services, to help improve and polish your everyday life"</h3>
       <h3>Contact Info</h3>
-      <p>Info</p>
+      <p>Phone: (828) 405-0462</p>
+      <p>Email: GT.Streamlined1@gmail.com</p>
+      <h2>Services We Offer</h2>
+      <div className="services">
+        <p>Lawn Mowing</p>
+        <p>Weedeating</p>
+        <p>Leaf Blowing</p>
+        <p>Hedge Trimming</p>
+        <p>Mulching</p>
+        <p>Yard Cleanup</p>
+        <p>Outdoor Fireplace Installation</p>
+        <p>Brush Removal</p>
+        <p>Fire Wood</p>
+        <p>Tree Removal</p>
+        <p>Trash Removal</p>
+        <p>Garden Bed Clearing</p>
+        <p>Leaf Clearing</p>
+        <p>Gutter Clearing</p>
+        <p>Roof Cleaning</p>
+        <p>Pressure Washing</p>
+        <p>Snow Removal</p>
+        <p>Irrigation Installation</p>
+        <p>Car Audio Upgrades</p>
+        <p>Porch Screening</p>
+      </div>
       {/* Contact Form */} 
     </div>
   );
